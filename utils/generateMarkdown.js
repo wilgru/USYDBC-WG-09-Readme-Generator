5a835a3f2e7ddd7998a43ee218de4ffdb5b63df5
wilgru <william.gruszka@gmail.com>
2022-04-21 17:12:16 +1000

    cleaned up code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -51,7 +51,6 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  console.log(license)
   if (!license) {
     return ""
   }
